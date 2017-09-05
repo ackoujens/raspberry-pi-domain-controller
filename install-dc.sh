36372d06da20adec45b17f79707beaa8022a112c
Ackou Jens <ackoujens@gmail.com>
2017-09-05 15:24:13 +0200

    If fix

--- install-dc.sh
+++ install-dc.sh
@@ -103,23 +103,21 @@ create_sudo_user() {
 lock_user() {
   locked_status=$(passwd -S pi | awk '{print $2;}')
   locked=$?
-  if [ ${locked_status} = "L" ] ; then
+  if [ ${locked_status} = "L" ]; then
     locked=1
   else
     locked=0
   fi
 
-  if [ ${locked} = 1 ] then
+  if [ ${locked} = 1 ]; then
     if whiptail --yesno "Are you sure you want to unlock the pi user account?" 0 0; then
       sudo passwd --unlock pi
-      echo "unlocked"
     fi
   fi
 
-  if [ ${locked} = 0 ] then
+  if [ ${locked} = 0 ]; then
     if whiptail --yesno "Are you sure you want to lock the pi user account?" 0 0; then
       sudo passwd --lock pi
-      echo "locked"
     fi
   fi
 }
