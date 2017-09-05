c1b071a85cc9d6462340560c43179299e406908f
Ackou Jens <ackoujens@gmail.com>
2017-09-05 15:21:20 +0200

    encapsulate if statement conditions

--- install-dc.sh
+++ install-dc.sh
@@ -109,14 +109,14 @@ lock_user() {
     locked=0
   fi
 
-  if ${locked} = 1 then
+  if [ ${locked} = 1 ] then
     if whiptail --yesno "Are you sure you want to unlock the pi user account?" 0 0; then
       sudo passwd --unlock pi
       echo "unlocked"
     fi
   fi
 
-  if ${locked} = 0 then
+  if [ ${locked} = 0 ] then
     if whiptail --yesno "Are you sure you want to lock the pi user account?" 0 0; then
       sudo passwd --lock pi
       echo "locked"
